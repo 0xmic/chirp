8e5c27b300491b69e4bfa1c2506992f3cc838905
Michael Longoria <michaelandrewlongoria@gmail.com>
2023-04-01 22:02:13 -0700

    Add Layout component

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -13,9 +13,9 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
       <Head>
         <title>{data.username}</title>
       </Head>
-      <main className="flex h-screen justify-center">
+      <PageLayout>
         <div>{data.username}</div>
-      </main>
+      </PageLayout>
     </>
   );
 };
@@ -24,6 +24,7 @@ import { createProxySSGHelpers } from "@trpc/react-query/ssg";
 import { appRouter } from "~/server/api/root";
 import { prisma } from "~/server/db";
 import superjson from "superjson";
+import { PageLayout } from "~/components/layout";
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const ssg = createProxySSGHelpers({
