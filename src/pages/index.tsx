46a0f0f18aad20f82b11297922e1f266a5ffc658
Michael Longoria <michaelandrewlongoria@gmail.com>
2023-04-02 11:48:38 -0700

    Add Profile page with ssg helper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { SignInButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
-import Head from "next/head";
 import Image from "next/image";
 
 import { api } from "~/utils/api";
