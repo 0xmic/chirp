485220ba094da686dd82d26c747fe9a4132f01ad
Michael Longoria <michaelandrewlongoria@gmail.com>
2023-04-02 13:55:36 -0700

    Update PageLayout classes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
-import { SignInButton, useUser } from "@clerk/nextjs";
+import { SignInButton, UserButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 
 import { api } from "~/utils/api";
 
-import Image from "next/image";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
 import toast from "react-hot-toast";
@@ -36,12 +35,15 @@ const CreatePostWizard = () => {
 
   return (
     <div className="flex w-full gap-3">
-      <Image
-        src={user.profileImageUrl}
-        alt="Profile image"
-        className="h-14 w-14 rounded-full"
-        width={56}
-        height={56}
+      <UserButton
+        appearance={{
+          elements: {
+            userButtonAvatarBox: {
+              width: 56,
+              height: 56,
+            },
+          },
+        }}
       />
       <input
         placeholder="Type some emojis"
@@ -98,7 +100,7 @@ const Home: NextPage = () => {
 
   return (
     <PageLayout>
-      <div>
+      <div className="flex border border-b border-slate-400 p-4">
         {!isSignedIn && (
           <div className="flex justify-center">
             <SignInButton />
